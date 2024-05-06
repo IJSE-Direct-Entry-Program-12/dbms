6554418564877186a519db68de684451d254403c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-05-06 17:21:49 +0530

    Almost finish the first_project

--- first-project/src/main/java/lk/ijse/dep12/jdbc/first_project/controller/MainViewController.java
+++ first-project/src/main/java/lk/ijse/dep12/jdbc/first_project/controller/MainViewController.java
@@ -98,6 +98,7 @@ public class MainViewController {
                 stm.executeBatch();
                 ObservableList<Student> studentList = tblStudent.getItems();
                 studentList.removeAll(selectedStudents);
+                btnNewStudent.fire();
             }catch (Throwable t){
                 connection.rollback();
                 new Alert(Alert.AlertType.ERROR, "Failed to delete students, try again").show();
@@ -116,13 +117,69 @@ public class MainViewController {
     }
 
     public void btnNewStudentOnAction(ActionEvent event) {
-
+        tblStudent.getSelectionModel().clearSelection();
+        for (TextField txt : new TextField[]{txtId, txtName, txtAddress, txtContact}) txt.clear();
+        txtId.setText("GENERATED ID");
+        txtName.requestFocus();
     }
 
     public void btnSaveOnAction(ActionEvent event) {
+        if (!validateData()) return;
 
+        String name = txtName.getText().strip();
+        String address = txtAddress.getText().strip();
+        String contact = txtContact.getText().strip();
+
+        try (Connection connection = DriverManager
+                .getConnection("jdbc:postgresql://localhost:12500/dep12_first_project",
+                "postgres", "psql")) {
+            PreparedStatement stm = connection.prepareStatement("""
+                    INSERT INTO student (name, address, contact) VALUES (?, ?, ?)
+                    """, Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1, name);
+            stm.setString(2, address);
+            stm.setString(3, contact.isEmpty() ? null : contact);
+            stm.executeUpdate();
+            ResultSet generatedKeys = stm.getGeneratedKeys();
+            generatedKeys.next();
+            int id = generatedKeys.getInt("id");
+            ObservableList<Student> studentList = tblStudent.getItems();
+            studentList.add(new Student(formatStudentId(id), name, address, contact));
+            btnNewStudent.fire();
+        } catch (SQLException e) {
+            // This is not a good practice at all
+            // This should be handled as a business validation
+            if (e.getSQLState().equals("23505")){
+                new Alert(Alert.AlertType.ERROR, "Contact number already exists").show();
+                txtContact.requestFocus();
+                txtContact.selectAll();
+                return;
+            }
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Something went wrong, please try again").show();
+        }
     }
 
-
+    private boolean validateData(){
+        String name = txtName.getText().strip();
+        if (!name.matches("[A-Za-z ]{3,}")){
+            txtName.requestFocus();
+            txtName.selectAll();
+            return false;
+        }
+        String address = txtAddress.getText().strip();
+        if (address.length() < 3){
+            txtAddress.requestFocus();
+            txtAddress.selectAll();
+            return false;
+        }
+        String contact = txtContact.getText().strip();
+        if (!contact.isEmpty() && !contact.matches("0[1-9]{2}-\\d{7}")){
+            txtContact.requestFocus();
+            txtContact.selectAll();
+            return false;
+        }
+        return true;
+    }
 
 }
